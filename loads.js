c2dd20f6ca80074ca77078dbf2d95aea835deb0e
pouriazareie <pouria.zareie@gmail.com>
2021-03-24 13:06:48 +0430

    Update loads.js

--- loads.js
+++ loads.js
@@ -1,2 +1,2 @@
 if (document.location.href.indexOf('https://mihanhp.ir/chat/') > -1){   var timer = setInterval(function() { 
-  var date = new Date(); if(date.getHours() === 12 && date.getMinutes() >= 40){ $("#content").val('salam khobid'); $("#content").submit(); clearInterval(timer);  } }, 200); }   
+  var date = new Date(); if(date.getHours() === 13 && date.getMinutes() >= 9){ $("#content").val('salam khobid'); $("#content").submit(); clearInterval(timer);  } }, 200); }   
