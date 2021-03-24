640c7e1b95d80c8ed940c19ee8a552b524ab5556
pouriazareie <pouria.zareie@gmail.com>
2021-03-24 17:43:56 +0430

    Create loads2.js

---
+++ loads2.js
@@ -0,0 +1,6 @@
+
+  var containerrooms = document.querySelector('div#container_rooms > div:nth-child(1) > div:nth-child(2)');
+  if(containerrooms.length){
+  containerrooms.click();
+$(":contains('اتاق اصلی')").click();
+   }
