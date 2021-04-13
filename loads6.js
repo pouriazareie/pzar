22532c0cc894cb79427803870152fa57267b3c81
pouriazareie <pouria.zareie@gmail.com>
2021-04-13 09:33:07 +0430

    Update loads6.js

--- loads6.js
+++ loads6.js
@@ -1,61 +1,34 @@
-
-function sleep(mss) {return new Promise(resolve => setTimeout(resolve, mss));}
-	function getRandomString(length) {
-    var randomChars = 'abcdefghijklmnopqrstuvwxyz';
-    var result = '';
-    for ( var i = 0; i < length; i++ ) {
-        result += randomChars.charAt(Math.floor(Math.random() * (randomChars.length)/20));
-    }
-    return result;
-}
-
 (async ()=>{
-try{
-var timer2 = setInterval( async function() {
-
 var date = new Date();
 
-  if( date.getMinutes() >= 0){ 
+  if(date.getHours()===9 && date.getMinutes()>=0 && date.getSeconds()>=0)
 
- for (var ii=0;ii<10000;)
-   {
-var v1 = performance.now();
- var date=new Date();
-let milisSec =date.getMilliseconds();
-let Sec =date.getSeconds();
-var finalresualt=getRandomString(((milisSec/Sec)/ 0.2));
-  ++ii
-  await sleep(  (20 +Sec)  * Math.random().toString(10).substr(2, 2))
+{ 
 
 
 
 
 
-/*  $("#content").val(finalresualt); */
-var v2 = performance.now();
-var dealys=(((v2-v1).toFixed(0))/1000).toFixed(0);
- if(finalresualt.length<100 && dealys>=1){
-        {
-console.log(finalresualt,finalresualt.length);
-	//	$("#content").val("aliz khafe kar kon toole sag");
 
-console.log(dealys);
-  //  $("#content").submit();
-}
+function doSomething() {
 
-  }
-}
-
-//clearInterval(timer2);
-	
-	}
-}
-}, 500);
-
+$("#content").val("tiger shire alireza rideeeeeeeeeeee"+Math.random().toString(10).substr(2, 2) );$("#content").submit();
 
 }
-catch(err)
-{
-	console.log(err.message);
+
+(function loop() {
+    var rand = Math.round(Math.random() *10);
+    setTimeout(function() {
+            doSomething();
+
+           // document.write("Delayed " + rand + " secs."+"\n"+"tiger shire alireza rideeeeeeeeeeee"+Math.random().toString(10).substr(2, 2) );
+
+
+
+            loop();  
+    }, rand*date.getMilliseconds());
+}());
+
+
 }
 })();
