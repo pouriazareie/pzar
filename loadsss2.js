31c35228635126d37088f4d1837c68b10571e8a9
pouriazareie <pouria.zareie@gmail.com>
2021-04-24 23:56:25 +0430

    Update loadsss2.js

--- loadsss2.js
+++ loadsss2.js
@@ -6,14 +6,16 @@ if ($('.room_element:nth-child(1)')){
 {
 console.log("roomnumber not exist");
   localStorage.setItem('roomnumber',1);
-}
 var myroomnumberss = localStorage.getItem("roomnumber");
 
+}
+
+
+$('div.room_element:nth-child(1)').click();
 
 
 console.log("existCondition4 Exists!!");
 
-document.querySelector("#container_rooms > div:nth-child(1) > div:nth-child(2) > div > div.room_name.centered_element").click();
 clearInterval(existCondition4);
 
 }
