ca519888d2cdf557402cae74000b9455f3c4c4d6
pouriazareie <pouria.zareie@gmail.com>
2021-04-26 03:13:42 +0430

    Update loadsss3.js

--- loadsss3.js
+++ loadsss3.js
@@ -1,27 +1,60 @@
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
 (async ()=>{
 
-var textsendss= localStorage.getItem("textsends");
 
+if( localStorage.getItem("textsend").length < 0)
+{
+ssconsole.log("textsend not exist");
+var textsendss= " k i r a m  d a h a n e t a l i r e z a ";
+
+}
+
+
+var arr=[];
+var date = new Date();
 
 var date = new Date();
 var timer2 = setInterval( async function() {
-  if($("#content").length )
-{ 
-function doSomething() {
-$("#content").val(textsendss+"  "+Math.random().toString(10).substr(2, 24) );$("#content").submit();
+  if( date.getMinutes() >= 1){ 
+
+ 
+ 
+function  doSomething() {
+var textsends ="☺☺☺";
+
+String.prototype.shuffle = function() {
+	  'use strict';
+	  var numberOfSpaces = Math.floor(Math.random() * (this.length - 1));
+	  var word = this;
+	  for (var i = 0; i < numberOfSpaces; i++) {
+		var index = Math.floor(Math.random() * (this.length - 1));
+		word = [word.slice(0,index), ' ', word.slice(index)].join('');
+	  }
+	  return word;
+	};
+
+
+
+
+
+		console.log(textsendss.shuffle(14,"salam"));
 }
 (function loop() {
 var date = new Date();
-    var rand = Math.round(Math.random() *7);
+    var rand = Math.round(Math.random() *10);
     setTimeout(function() {
       doSomething();
- console.log("Delayed " + rand + " secs." );
+ console.log("Delayed " + rand + " Secs" );
 loop();  
     }, rand*date.getMilliseconds());
 }());
+
+
 clearInterval(timer2);
-  console.log("injected ");
-}
-}, 1000);
+
+
+console.log("injected ");
+
+  }
+}, 2000);
 })();
