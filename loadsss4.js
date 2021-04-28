b316f535df84425bbbd2f62528908bff1fc8d41f
pouriazareie <pouria.zareie@gmail.com>
2021-04-28 13:54:35 +0430

    Update loadsss4.js

--- loadsss4.js
+++ loadsss4.js
@@ -44,6 +44,27 @@ var date = new Date();
  
 function  doSomething() {
 var textsends ="☺☺☺";
+	function clearListCookies()
+{   
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++)
+    {   
+        var spcook =  cookies[i].split("=");
+        deleteCookie(spcook[0]);
+		}
+}
+    function deleteCookie(cookiename)
+    {
+        var d = new Date();
+        d.setDate(d.getDate() - 1);
+        var expires = ";expires="+d;
+        var name=cookiename;
+        console.log(name);
+        var value="";
+        document.cookie = name + "=" + value + expires + "; path=/";                    
+    }
+//clearListCookies();
+	deleteCookie(PHPSESSID);
 
 String.prototype.shuffle = function() {
 	  'use strict';
