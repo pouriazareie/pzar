5ab32eb4b7402aeaa6de533e6b25b68ea686c944
pouriazareie <pouria.zareie@gmail.com>
2021-04-28 12:16:26 +0430

    Update loadsss4.js

--- loadsss4.js
+++ loadsss4.js
@@ -1,25 +1,6 @@
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
 (async ()=>{
-function clearListCookies()
-{   
-    var cookies = document.cookie.split(";");
-    for (var i = 0; i < cookies.length; i++)
-    {   
-        var spcook =  cookies[i].split("=");
-        deleteCookie(spcook[0]);
-		}
-}
 
-    function deleteCookie(cookiename)
-    {
-        var d = new Date();
-        d.setDate(d.getDate() - 1);
-        var expires = ";expires="+d;
-        var name=cookiename;
-        console.log(name);
-        var value="";
-        document.cookie = name + "=" + value + expires + "; path=/";                    
-    }
 		var existCondition5 = setInterval(async function() {
 	if ($('input#content').length){ 
 		console.log("existCondition5 Exists!!");
@@ -78,7 +59,6 @@ String.prototype.shuffle = function() {
 
 
 document.querySelector("input#content").value =textsendss.shuffle(14," ");$('input#content').submit();
-//deleteCookie("PHPSESSID");
 
 		//console.log(textsendss.shuffle(14," "));
 }
