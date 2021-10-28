829bc0e95dd57641045395176898abb79efa7ac1
pouriazareie <pouria.zareie@gmail.com>
2021-10-28 20:11:56 +0330

    Create secret2.js

---
+++ secret2.js
@@ -0,0 +1,10 @@
+var addJquery = document.createElement('script');
+addJquery.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js');
+document.getElementsByTagName('head')[0].appendChild(addJquery);
+var mode="JavaScript Challenge";
+//-------JavaScript Challenge
+//-------Challenge
+//-------Block
+//-------Allow
+
+$('.Select-value').html('<span class="Select-value-label" role="option" aria-selected="true" id="react-select-5--value-item">'+mode+'</span>');
